abf1f1d3074c2ba45c940e514a1f7ac4c6c36fbc
fakheet <27400@protonmail.ch>
2021-07-17 19:31:16 +0600

    fix artist and event interfaces declaration

--- src/app/interfaces/artist.ts
+++ src/app/interfaces/artist.ts
@@ -1,16 +1,16 @@
 export interface Artist {
-  'thumb_url': string;
-  'mbid': string;
-  'support_url': string;
-  'facebook_page_url': string;
-  'image_url': string;
-  'name': string;
-  'options': {
+  thumb_url: string;
+  mbid: string;
+  support_url: string;
+  facebook_page_url: string;
+  image_url: string;
+  name: string;
+  options: {
     'display_listen_unit': boolean;
   };
-  'id': string;
-  'tracker_count': number;
-  'upcoming_event_count': number;
-  'url': string;
+  id: string;
+  tracker_count: number;
+  upcoming_event_count: number;
+  url: string;
 }
 
